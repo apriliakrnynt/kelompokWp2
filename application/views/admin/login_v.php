afa39adaed9111eeac985a81f3996e1edfbe436b
Dewianing <90625464+Dewianing@users.noreply.github.com>
2021-12-23 11:43:59 +0700

    Dewi Aning - 19200323

---
+++ application/views/admin/login_v.php
@@ -0,0 +1,46 @@
+<?php
+$meta = $this->menu_m->select_meta()->row();
+?>
+<div class="login-container lightmode">
+    <div class="login-box animated fadeInDown">
+      
+            <div class="login-title"><strong>Log In</strong> FOOD RESTO</div>
+            <?php
+            if ($this->session->flashdata('notification')) {
+            ?>
+            <div class="alert alert-danger" align="center">
+                <?=$this->session->flashdata('notification');?>
+            </div>
+            <?php
+            }
+            ?>
+            <form action="<?=site_url('login/validasi');?>" class="form-horizontal" method="post">
+            <div class="form-group">
+                <div class="col-md-12">
+                    <input type="text" class="form-control" name="username" placeholder="Username" pattern="^[a-zA-Z0-9-_\.]{1,30}$" title="Jangan Pakai SPASI" minlength="5" autocomplete="off" required autofocus>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-md-12">
+                    <input type="password" class="form-control" name="password" placeholder="Password" autocomplete="off" required/>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-md-6">
+                    
+                </div>
+                <div class="col-md-6">
+                    <button class="btn btn-danger btn-block"><i class="fa fa-sign-in"></i> Login</button>
+                </div>
+            </div>
+            </form>
+            <br><center><p style="color: black;">Repost by <a style="color: black;" href='https://FoodResto.com/' title='FoodResto.com' target='_blank'>FoodResto.com</a></p></center>
+            
+        </div>
+        <div class="login-footer">
+            <div class="pull-left">
+            
+            </div>
+        </div>
+    </div>
+</div>
---
+++ application/views/template/footer.php
@@ -0,0 +1,12 @@
+<?php
+$meta = $this->menu_m->select_meta()->row();
+?>
+<div class="page-footer">
+	<div class="page-footer-inner">
+		<?=date('Y'); ?> &copy; <?=$meta->meta_name;?> | Repost by <a href='https://FoodResto.com/' target='_blank'>UBSI KALIABANG</a>
+		
+	</div>
+	<div class="scroll-to-top">
+	    <i class="icon-arrow-up"></i>
+	</div>
+</div>
